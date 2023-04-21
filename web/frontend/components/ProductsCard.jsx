b05ea14501bdf055ce3fdb735cd78763ebfc3ec0
Matt Taggart <mtaggart89@gmail.com>
2023-04-21 15:29:59 -0700

    clean up entry point

--- web/frontend/components/ProductsCard.jsx
+++ web/frontend/components/ProductsCard.jsx
@@ -7,14 +7,11 @@ import {
   TextField,
   Pagination,
 } from "@shopify/polaris";
-import { Toast } from "@shopify/app-bridge-react";
-import { useAppQuery, useAuthenticatedFetch } from "../hooks";
+import { useAppQuery } from "../hooks";
 
 export function ProductsCard() {
-  const emptyToastProps = { content: null };
   const [isLoading, setIsLoading] = useState(true);
-  const [toastProps, setToastProps] = useState(emptyToastProps);
-  const fetch = useAuthenticatedFetch();
+  const [value, setValue] = useState("");
 
   const {
     data,
@@ -29,27 +26,6 @@ export function ProductsCard() {
       },
     },
   });
-
-  const toastMarkup = toastProps.content && !isRefetchingCount && (
-    <Toast {...toastProps} onDismiss={() => setToastProps(emptyToastProps)} />
-  );
-
-  const handlePopulate = async () => {
-    setIsLoading(true);
-    const response = await fetch("/api/products/create");
-
-    if (response.ok) {
-      await refetchProductCount();
-      setToastProps({ content: "5 products created!" });
-    } else {
-      setIsLoading(false);
-      setToastProps({
-        content: "There was an error creating products",
-        error: true,
-      });
-    }
-  };
-  const [value, setValue] = useState("");
 
   // const { data, isLoading, isRefetching } = useAppQuery({
   //   url: `/api/products`,
